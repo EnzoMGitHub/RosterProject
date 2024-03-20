c5ab275a471c9a2cac8598269f481d77f741d452
EnzoMGitHub <vincenzomonterosso9@gmail.com>
2024-03-20 13:53:15 -0400

    IP: Converting .pack to .grid

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from PIL import Image, ImageTk
 import os
+import math
 
 
 
@@ -17,25 +18,25 @@ class gui():
 
         root.minsize(height=600,width=800)
         frame = Frame(root,bg='light blue')
-        frame.pack(expand=YES, fill=BOTH)
+        frame.pack(fill=BOTH)
 
         title = Label(frame, text='Select a Team to Begin', bg='light blue', fg='black', font=('Arial', 20))
-        title.pack(side=TOP)
+        title.grid(row=0,column=1)
 
         # photo = PhotoImage(file='/Users/enzom/code/Github_Private_Projects/RosterProject/Screenshot 2024-02-22 at 8.38.51 PM copy.png')
         # button = Button(frame, image=photo, compound=LEFT,bg='light blue')
         # button.pack()
-
-        canvas = Canvas(frame, width=300, height=300, bg='light blue')
-        canvas.pack()   
-
-        directory = os.path.join('/Users/enzom/code/Github_Private_Projects/RosterProject','bills.png')
-        button_image = Image.open(directory)
-        bills = Button(root,image=button_image) 
-        button_photo = ImageTk.PhotoImage(button_image)
-
-        button = canvas.create_image(50, 50, image=button_photo, anchor=NW)
-        canvas.tag_bind(button, "<Button-1>", billsClick)
+        teams = ['Arizona Cardinals','Atlanta Falcons','Baltimore Ravens', 'Buffalo Bills', 'Carolina Panthers', 'Chicago Bears' ,'Cincinnati Bengals' ,'Cleveland Browns', \
+                'Dallas Cowboys', 'Denver Broncos', 'Detroit Lions', 'Green Bay Packers', 'Houston Texans', 'Indianapolis Colts' \
+                , 'Jacksonville Jaguars', 'Kansas City Chiefs', 'Las Vegas Raiders', 'Los Angeles Chargers', 'Los Angeles Rams', \
+                 'Miami Dolphins', 'Minnesota Vikings', 'New England Patriots', 'New Orleans Saints', 'New York Giants', 'New York Jets', 'Philadelphia Eagles' \
+                , 'Pittsburgh Steelers', 'San Francisco 49ers', 'Seattle Seahawks', 'Tampa Bay Buccaneers', 'Tennessee Titans Washington Commanders']
+        count = 0
+        button_Frame = Frame(root)
+        button_Frame.grid
+        for i in teams:
+            Button(button_Frame,text=i).grid(row=math.floor(count/5), column=(count%5)+1)
+            count += 1
 
     def clean():
         pass
